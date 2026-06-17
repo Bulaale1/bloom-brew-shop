af3c507e41567d4024c76f1231adb0f5126ff108
Yasin Hussein <bulaale116@gmail.com>
2026-06-17 08:10:02 -0600

    core installed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const menuRoutes = require('./src/routes/menu.routes');
 const ordersRoutes = require('./src/routes/orders.routes');
+const cors = require('cors');
 
 // (A) - App initialization
 const app = express();
@@ -13,7 +14,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); // Standard practice to group static serving with middlewares
-
+app.use(cors());
 // (E) - Endpoints / Routes
 app.get('/', (req, res) => {
   res.json('Hello! the server is working!');
